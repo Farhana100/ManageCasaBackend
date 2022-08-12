29d31583e881c0870af6799c2fc317a3a0434848
Farhana100 <farhanakhanhp@gmail.com>
2022-08-12 11:54:12 +0600

    error resolved

--- user/models.py
+++ user/models.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 from django.db import models
 from django.contrib.auth.models import User
 
-from apartment.models import Apartment
+# from apartment.models import Apartment
 
 
 def get_owner_image_upload_path(instance, name):
